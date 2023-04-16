8d0faafffb202415f28f7352a0d6a89ac2aae331
Paulaner99 <rikipaolini99@studio.unibo.it>
2023-04-16 11:57:30 +0200

    recreated masks

--- new/utils/resize_masks.py
+++ new/utils/resize_masks.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-dataset = 'yellowdino_new_new'
+dataset = 'greendino_new'
 invert = True
 
 datasets_folder = os.path.join(os.getcwd(), 'datasets')
@@ -15,16 +15,20 @@ h = cv2.imread(image).shape[0]
 w = cv2.imread(image).shape[1]
 
 for mask in os.listdir(mask_folder):
-    mask_path = os.path.join(mask_folder, mask)
-    m = cv2.imread(mask_path)
-    m = cv2.resize(m, (h, w))
-
-    if invert:
-        _, m = cv2.threshold(m, 255//2, 255, cv2.THRESH_BINARY_INV)
-    else:
-        _, m = cv2.threshold(m, 255//2, 255, cv2.THRESH_BINARY)
-    m = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)
     name, ext = mask.split('.')
-    print(os.path.join(mask_folder, name + '.jpg'))
-    cv2.imwrite(os.path.join(mask_folder, name + '.jpg'), m)
+    if ext == 'png':
+        mask_path = os.path.join(mask_folder, mask)
+        m = cv2.imread(mask_path)
+        m = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)
+        m = cv2.resize(m, (h, w))
+
+        if invert:
+            _, m = cv2.threshold(m, 255//2, 255, cv2.THRESH_BINARY_INV)
+        else:
+            _, m = cv2.threshold(m, 255//2, 255, cv2.THRESH_BINARY)
+
+        name, ext = mask.split('.')
+        print(os.path.join(mask_folder, name + '.jpg'))
+        cv2.imwrite(os.path.join(mask_folder, name + '.jpg'), m)
+
     
